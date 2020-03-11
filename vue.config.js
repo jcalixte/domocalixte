406d1176a6de9e06015018c4e9b7c0e9914b585a
Julien Calixte <julienc@bam.tech>
2020-03-12 00:32:22 +0100

    :wrench: (pwa) update manifest on vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -6,6 +6,26 @@ module.exports = {
       skipWaiting: true,
       clientsClaim: true
     },
-    name: 'Domotique Calixte'
+    manifestOptions: {
+      name: 'Au 72',
+      short_name: 'Au 72',
+      icons: [
+        {
+          src: './img/icons/android-chrome-192x192.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: './img/icons/android-chrome-512x512.png',
+          sizes: '512x512',
+          type: 'image/png'
+        }
+      ],
+      start_url: './index.html',
+      display: 'standalone',
+      background_color: '#ffffff',
+      theme_color: '#94c1b6'
+    },
+    name: 'Au 72'
   }
 }
